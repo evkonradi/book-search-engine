6d5d4e14d50f9fd5716c5b1aea6536097a58ac13
ElK <62822011+evkonradi@users.noreply.github.com>
2020-10-10 16:59:06 -0500

    Apollo Server; auth middleware; schema directory; typeDefs are completed

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,18 @@
+const { User, Book } = require('../models');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user){
+                const userData = await User.findOne({_id: context.user._id})
+                .select('-__v -password')
+          
+              return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+    Mutation: {
+
+    }
+}
